aaadf0713b0f2f9881cdbc70b0e2cd1069408236
LeVietThanh1412 <thanhdevuetvnu2024@gmail.com>
2026-09-07 09:53:33 +0700

    First commit

--- Source/Ship_Real/Ship_Real.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Ship_Real.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ship_Real, "Ship_Real" );
